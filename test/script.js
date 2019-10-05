5c29531f65eb48e0bdff6803912a688ab9596d7f
Chanayus  Nuiad <40952652+chanayus@users.noreply.github.com>
2019-10-05 11:35:23 +0700

    Add files via upload

--- test/script.js
+++ test/script.js
@@ -1,3 +1,5 @@
+let nightmode_enable = false;
+
 function white_excel(x){
     x.src = "image/white_excel.png";
 
@@ -11,10 +13,24 @@ function green_excel(x){
 
 function openNav() {
     document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+
 }
 
 function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+
+}
+
+function nightmode(){
+    if(nightmode_enable == false){
+        document.body.style.backgroundColor ="#333";
+        document.getElementById("text").style.color ="#fff";
+        nightmode_enable = true;
+    }
+    else{
+        document.body.style.backgroundColor ="#b5bdc5";
+        document.getElementById("text").style.color ="#333";
+        nightmode_enable = false;
+    }
+   
 }

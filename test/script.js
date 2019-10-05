40aac8f8d33794ffe77f7985c5fec3729fd281ea
t <38640424+poohcid@users.noreply.github.com>
2019-10-05 11:29:06 +0700

    test
    test

---
+++ test/script.js
@@ -0,0 +1,20 @@
+function white_excel(x){
+    x.src = "image/white_excel.png";
+
+}
+
+
+function green_excel(x){
+    x.src = "image/excel.png";
+
+}
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}

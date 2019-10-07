a4b9661a1f1643f1843f31c91d7c9f3dd7fc1e14
Chanayus  Nuiad <40952652+chanayus@users.noreply.github.com>
2019-10-07 20:41:50 +0700

    Add files via upload

--- test/script.js
+++ test/script.js
@@ -16,13 +16,14 @@ function closeNav() {
 
 function nightmode(){
     if(nightmode_enable == false){
-        document.body.style.backgroundColor ="#333";
+        document.body.style.backgroundColor ="#111";
         document.getElementById("text").style.color ="#fff";
+        document.getElementById("content1").style.backgroundColor="#333";
         nightmode_enable = true;
     }
     else{
         document.body.style.backgroundColor ="#b5bdc5";
-        document.getElementById("text").style.color ="#333";
+        document.getElementById("text").style.color ="#111";
         nightmode_enable = false;
     }
 }
@@ -30,7 +31,7 @@ function nightmode(){
 
 function changeback(){
     img = document.getElementById("background");
-
+    img.onclick = "https://google.com";
     
     if(change_count == 0){
         img.style.backgroundImage = "url('image/back1.png')";
@@ -38,6 +39,7 @@ function changeback(){
     }
     else if(change_count == 1){
         img.style.backgroundImage = "url('image/back2.png')";
+       
         change_count = 2;
     }
     else if(change_count == 2){
@@ -49,4 +51,24 @@ function changeback(){
         change_count = 0;
     }
     
+}
+
+function background_link(){ 
+    if(change_count == 0){
+        alert('1');
+        window.location.href = 'https://facebook.com';
+    }
+    else if(change_count == 1){
+        alert('2');
+        window.open("https://www.youtube.com");
+    }
+    else if(change_count == 2){
+        alert('3');
+        window.open("https://www.google.com");
+    }
+    else if(change_count == 3){
+        alert('4');
+        window.open("https://www.instagram.com");
+    }
+
 }

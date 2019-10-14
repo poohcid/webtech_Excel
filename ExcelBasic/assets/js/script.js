33a4b6ac38c439515c0e2606bfb8dc22d3bdc6c4
chanayus <chanayus1526@hotmail.com>
2019-10-14 11:27:57 +0700

    chanayus
    Update 14/10/2019
    *Update Page Unit 2
    *Update Page Unit2.1 and 2.2

--- ExcelBasic/assets/js/script.js
+++ ExcelBasic/assets/js/script.js
@@ -19,9 +19,12 @@ function closeNav() {
 function nightmode(){
     if(nightmode_enable == false){
         document.body.style.backgroundColor ="#222";
-        document.getElementById("text").style.color ="#fff";
-        document.getElementById("content1").style.color ="#fff";
+        var text = document.querySelectorAll("#text");
         var line = document.querySelectorAll(".underline");
+
+        for (i = 0; i < text.length; i++) {
+            text[i].style.color = "#FFF";
+          }  
         for (i = 0; i < line.length; i++) {
             line[i].style.backgroundColor = "rgb(255, 191, 118)";
           }
@@ -29,9 +32,12 @@ function nightmode(){
     }
     else{
         document.body.style.backgroundColor ="#b5bdc5";
-        document.getElementById("text").style.color ="#000";
-        document.getElementById("content1").style.color ="#000";
+        var text = document.querySelectorAll("#text");
         var line = document.querySelectorAll(".underline");
+        
+        for (i = 0; i < text.length; i++) {
+            text[i].style.color = "#000";
+          }      
         for (i = 0; i < line.length; i++) {
             line[i].style.backgroundColor = "#111";
           }
